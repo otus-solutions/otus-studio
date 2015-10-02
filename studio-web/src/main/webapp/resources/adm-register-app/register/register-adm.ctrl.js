3b938e3ff2486ca68e215a608aad62914958474b
fagner <fagnerschwalm@gmail.com>
2015-10-02 15:15:03 -0300

    Add message to redirect to login page.

--- studio-web/src/main/webapp/resources/adm-register-app/register/register-adm.ctrl.js
+++ studio-web/src/main/webapp/resources/adm-register-app/register/register-adm.ctrl.js
@@ -1,13 +1,31 @@
-angular.module('RegisterAdm', ['ngMaterial', 'ui.mask']).controller('RegisterAdmCtrl', function($scope, $http, $window) {
+angular.module('RegisterAdm', ['ngMaterial', 'ui.mask']).controller('RegisterAdmCtrl', function($scope, $http, $window, $mdDialog) {
  
 	/**
-   * REST URL: /register/adm
-   */
+	 * REST URL: /register/adm
+	 */
+	
+	
+function confirmAlertToNavigate() {
+	      alert = $mdDialog.alert()
+	        .title('Informação')
+	        .content('Seu cadastro foi realizado com sucesso! Você vai ser redirecionado para a tela de login.')
+	        .ok('ok');
+
+	      $mdDialog
+	          .show( alert )
+	          .finally(function() {
+	            $window.location.href = 'http://localhost:8080/studio/'
+	          });
+}
+	
+	
+	
+	
   $scope.register = function (user) {
     $http.post('http://localhost:8080/studio/session/rest/register/adm', user).then(function(response){
-    	console.log(user.name)
-        console.log("Sucess!!!")
-        $window.location.href = 'http://localhost:8080/studio/'
+    	
+    	confirmAlertToNavigate();
+    	
     },
     function(response){
     	console.log(user)
