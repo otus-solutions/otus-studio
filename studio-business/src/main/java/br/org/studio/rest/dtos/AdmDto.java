c149d131b657a65a140318fba0472157ef15ddf7
Emanoel Vianna <emanoel.vianna@outlook.com>
2015-10-02 14:52:31 -0300

    Modificações para criptografia do usuário

--- studio-business/src/main/java/br/org/studio/rest/dtos/AdmDto.java
+++ studio-business/src/main/java/br/org/studio/rest/dtos/AdmDto.java
@@ -1,10 +1,13 @@
 package br.org.studio.rest.dtos;
 
+import br.org.studio.security.EncryptorResources;
 import br.org.tutty.Equalization;
 
 import java.io.Serializable;
 
 public class AdmDto implements Serializable{
+
+	private static final long serialVersionUID = -5851680952577534837L;
 
 	@Equalization(name = "name")
 	private String name;
@@ -20,4 +23,8 @@ public class AdmDto implements Serializable{
 
 	@Equalization(name = "password")
 	private String password;
+	
+	public void encrypt(){
+		this.password = EncryptorResources.encrypt(password);
+	}
 }
