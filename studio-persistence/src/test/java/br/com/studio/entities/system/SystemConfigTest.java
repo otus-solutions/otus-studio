9ffa3581e00e0fe0421041f6faeb618995abb0a4
Emanoel Vianna <emanoel.vianna@outlook.com>
2015-10-02 15:32:51 -0300

    con-9

--- studio-persistence/src/test/java/br/com/studio/entities/system/SystemConfigTest.java
+++ studio-persistence/src/test/java/br/com/studio/entities/system/SystemConfigTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 
 import br.org.studio.entities.system.SystemConfig;
 
