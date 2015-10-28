70ee85865d6f95a21a0244ef457784768c27e486
carolinanery <carolina.nery94@gmail.com>
2015-10-28 16:58:03 -0200

    USR-12-1 Criar mecanismo para Realizar login (parcial).

---
+++ studio-web/src/main/java/br/org/studio/rest/UserAccessLogin.java
@@ -0,0 +1,25 @@
+package br.org.studio.rest;
+
+import javax.inject.Inject;
+import javax.naming.AuthenticationException;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("studio")
+public class UserAccessLogin {
+
+	@Inject
+	private UserResouce userResouce;
+	private HttpServletRequest httpServletRequest;
+
+	public UserAccessLogin(UserResouce userResouce, HttpServletRequest httpServletRequest) {
+
+		this.userResouce = userResouce;
+		this.httpServletRequest = httpServletRequest;
+	}
+
+	public User authenticatorLogin(String userName, String password) throws AuthenticationException {
+
+	}
+
+}
