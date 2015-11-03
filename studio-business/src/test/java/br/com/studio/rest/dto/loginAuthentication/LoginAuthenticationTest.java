e5654eee65b5cd587891fd3b0162fd24f738d25f
carolinanery <carolina.nery94@gmail.com>
2015-11-03 15:06:36 -0200

    commit parcial dos testes USR-12 LoginAuthenticationTest

--- studio-business/src/test/java/br/com/studio/rest/dto/loginAuthentication/LoginAuthenticationTest.java
+++ studio-business/src/test/java/br/com/studio/rest/dto/loginAuthentication/LoginAuthenticationTest.java
@@ -1,27 +1,51 @@
 package br.com.studio.rest.dto.loginAuthentication;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import br.org.studio.dao.UserDao;
+import br.org.studio.entities.system.User;
+import br.org.studio.exception.EmailNotFoundException;
+import br.org.studio.exception.InvalidPasswordException;
+import br.org.studio.exceptions.DataNotFoundException;
 import br.org.studio.rest.dtos.LoginAuthenticationDto;
+import br.org.studio.security.EncryptorResources;
+import br.org.studio.security.SecurityServiceBean;
 
-import com.google.gson.Gson;
-
+@RunWith(MockitoJUnitRunner.class)
 public class LoginAuthenticationTest {
+	
+	private static final String EMAIL = "joao.silveira@gmail.com";
+	private static final String PASSWORD = "158927&";
+	private static final String ENCRYPT_PASSWORD = "5f4dcc3b5aa765d61d8327deb882cf99";
 
+	
+	@InjectMocks
 	private LoginAuthenticationDto login;
 	
-	@Before
-	public void setup(){
-		login = new LoginAuthenticationDto();
-		login.setEmail("emailTest");
-		login.setPassword("passwordTest");
-	}
+	@Mock
+	private UserDao userDao;
+	@Mock
+	private SecurityServiceBean securityService;
+	@Mock
+	private User user;
+	@Mock
+	private SecurityServiceBean service;
+	@Mock
+	private EncryptorResources encrypt;
+
 	
 	@Test
-	public void toString_should_return_a_json_with_value_of_data_equal_to_true(){
-		Gson gson = new Gson();
-		login generatedJson = gson.fromJson();
+	public void encrypt_password_success() throws DataNotFoundException, InvalidPasswordException, EmailNotFoundException{
+		Mockito.when(userDao.fetchByEmail(EMAIL)).thenReturn(user);
+		Mockito.when(user.getPassword()).thenReturn(PASSWORD);
+		
+		Mockito.verify(login).encryptPassword();
 		
 	}
+
 }
