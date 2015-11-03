fb73b046649a51f9804caa0fcbf3ba96248b4144
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2015-11-03 13:49:19 -0200

    USR-12 Rest para Controle de Usuarios

---
+++ studio-web/src/test/java/br/org/studio/rest/AdministrationUsersResourceTest.java
@@ -0,0 +1,69 @@
+package br.org.studio.rest;
+
+import br.org.studio.administration.AdministrationUserServiceBean;
+import br.org.studio.rest.dtos.UserDto;
+import br.org.studio.rest.dtos.administration.AdministrationUser;
+import com.google.gson.Gson;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by diogoferreira on 03/11/15.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class AdministrationUsersResourceTest {
+
+    @InjectMocks
+    private AdministrationUsersResource administrationUsersResource = new AdministrationUsersResource();
+
+    @Mock
+    private AdministrationUserServiceBean administrationUserServiceBean;
+
+    @Test
+    public void getUsers_should_fetch_administrationUser_to_json(){
+        AdministrationUser administrationUser = new AdministrationUser();
+
+        Mockito.when(administrationUserServiceBean.fetchUsers()).thenReturn(administrationUser);
+
+        String users = administrationUsersResource.getUsers();
+
+        Assert.assertEquals(users, new Gson().toJson(administrationUser));
+    }
+
+    @Test
+    public void disableUsers_should_disable_users_using_service(){
+        List<UserDto> users = new ArrayList<>();
+        users.add(new UserDto());
+        users.add(new UserDto());
+
+        String usersJson = new Gson().toJson(users);
+
+        String result = administrationUsersResource.disableUsers(usersJson);
+        Mockito.verify(administrationUserServiceBean).disableUsers(Matchers.anyList());
+
+        Assert.assertTrue(new Gson().fromJson(result, Boolean.class));
+    }
+
+    @Test
+    public void enableUsers_should_enable_users_using_service(){
+        List<UserDto> users = new ArrayList<>();
+        users.add(new UserDto());
+        users.add(new UserDto());
+
+        String usersJson = new Gson().toJson(users);
+
+        String result = administrationUsersResource.enableUsers(usersJson);
+        Mockito.verify(administrationUserServiceBean).enableUsers(Matchers.anyList());
+
+        Assert.assertTrue(new Gson().fromJson(result, Boolean.class));
+    }
+}
