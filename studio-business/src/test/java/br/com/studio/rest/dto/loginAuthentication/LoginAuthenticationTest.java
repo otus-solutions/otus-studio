f5611b174aed5bbf5e3b7f65d419b21785129e00
carolinanery <carolina.nery94@gmail.com>
2015-11-04 12:25:56 -0200

    USR-12 testes finalizados rest.

--- studio-business/src/test/java/br/com/studio/rest/dto/loginAuthentication/LoginAuthenticationTest.java
+++ studio-business/src/test/java/br/com/studio/rest/dto/loginAuthentication/LoginAuthenticationTest.java
@@ -1,50 +1,33 @@
 package br.com.studio.rest.dto.loginAuthentication;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import br.org.studio.dao.UserDao;
-import br.org.studio.entities.system.User;
 import br.org.studio.exception.EmailNotFoundException;
 import br.org.studio.exception.InvalidPasswordException;
 import br.org.studio.exceptions.DataNotFoundException;
 import br.org.studio.rest.dtos.LoginAuthenticationDto;
-import br.org.studio.security.EncryptorResources;
-import br.org.studio.security.SecurityServiceBean;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LoginAuthenticationTest {
 	
 	private static final String EMAIL = "joao.silveira@gmail.com";
 	private static final String PASSWORD = "158927&";
-	private static final String ENCRYPT_PASSWORD = "5f4dcc3b5aa765d61d8327deb882cf99";
+	private static final String ENCRYPT_PASSWORD = "LXsrveNk8J7x7+t932NAERzKGFc=";
 
-	
-	@InjectMocks
 	private LoginAuthenticationDto login;
 	
-	@Mock
-	private UserDao userDao;
-	@Mock
-	private SecurityServiceBean securityService;
-	@Mock
-	private User user;
-	@Mock
-	private SecurityServiceBean service;
-	@Mock
-	private EncryptorResources encrypt;
 
-	
 	@Test
 	public void encrypt_password_success() throws DataNotFoundException, InvalidPasswordException, EmailNotFoundException{
-		Mockito.when(userDao.fetchByEmail(EMAIL)).thenReturn(user);
-		Mockito.when(user.getPassword()).thenReturn(PASSWORD);
+		login = new LoginAuthenticationDto(EMAIL, PASSWORD);
 		
-		Mockito.verify(login).encryptPassword();
+		login.encryptPassword();
+		
+		assertEquals(ENCRYPT_PASSWORD, login.getPassword());
 		
 	}
 
