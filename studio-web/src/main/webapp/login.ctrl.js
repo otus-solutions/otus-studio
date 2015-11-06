37e9bef88eca9d1eb3a015081cf76278ab8dc207
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2015-11-06 11:16:36 -0200

    USR-11 Adição de botão para cadastramento de usuario

--- studio-web/src/main/webapp/login.ctrl.js
+++ studio-web/src/main/webapp/login.ctrl.js
@@ -1,7 +1,12 @@
-angular.module('Login', ['ngMaterial', 'ui.mask']).controller('LoginController', function($scope){
-	
+angular.module('Login', ['ngMaterial', 'ui.mask']).controller('LoginController', function($scope, $window){
+    var HTTP_URL_REGISTER_PAGE = window.location.origin + '/studio/resources/otus-studio-app/administration/register/user/register-user.html';
+
 	$scope.logar = function (user) {
 		console.log($scope.user);
 	}
-	
+
+    $scope.register = function (){
+        $window.location.href = HTTP_URL_REGISTER_PAGE;
+    }
+
 });
