23d5f4078d289e7379ad045c3a5b7fde587670e5
fagner <fagnerschwalm@gmail.com>
2015-11-06 14:51:22 -0200

    Create DTOS to initial config.

---
+++ studio-business/src/main/java/br/org/studio/rest/dtos/EmailSenderDto.java
@@ -0,0 +1,21 @@
+package br.org.studio.rest.dtos;
+
+import br.org.studio.security.EncryptorResources;
+import br.org.tutty.Equalization;
+
+public class EmailSenderDto {
+	
+	@Equalization(name = "name")
+	private String name;
+
+	@Equalization(name = "email")
+	private String email;
+
+	@Equalization(name = "password")
+	private String password;
+	
+	public void encrypt() {
+		this.password = EncryptorResources.encrypt(password);
+	}
+
+}
