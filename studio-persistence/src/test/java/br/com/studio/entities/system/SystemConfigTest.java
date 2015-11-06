6a9444ae62f43ca0c606de31113386009d8884ca
fagner <fagnerschwalm@gmail.com>
2015-11-06 14:50:17 -0200

    Ad relationship with EmailSender.

--- studio-persistence/src/test/java/br/com/studio/entities/system/SystemConfigTest.java
+++ studio-persistence/src/test/java/br/com/studio/entities/system/SystemConfigTest.java
@@ -2,15 +2,25 @@ package br.com.studio.entities.system;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 
+import br.org.studio.entities.email.EmailSender;
 import br.org.studio.entities.system.SystemConfig;
 
 public class SystemConfigTest {
 
 	@InjectMocks
-	private SystemConfig systemConfig = new SystemConfig();
+	private SystemConfig systemConfig;
+
+	@InjectMocks
+	private EmailSender emailSender;
+	
+	@Before
+	public void setUp(){
+		systemConfig = new SystemConfig(emailSender);
+	}
 
 	@Test
 	public void shouldTrueWhenFianlizeConfiguration() {
