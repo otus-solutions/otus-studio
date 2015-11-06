99d0f1be85a4109382fdd2a52200decd0fdc2f0a
fagner <fagnerschwalm@gmail.com>
2015-11-06 14:43:40 -0200

    Add method findAdmin in userDao.

--- studio-persistence/src/main/java/br/org/studio/dao/UserDao.java
+++ studio-persistence/src/main/java/br/org/studio/dao/UserDao.java
@@ -1,7 +1,12 @@
 package br.org.studio.dao;
 
+import br.org.studio.entities.system.User;
+import br.org.studio.exceptions.DataNotFoundException;
+
 public interface UserDao extends GenericDao {
 
     Boolean emailExists(String email);
+
+	User findAdmin() throws DataNotFoundException;
 
 }
--- studio-persistence/src/main/java/br/org/studio/dao/UserDaoBean.java
+++ studio-persistence/src/main/java/br/org/studio/dao/UserDaoBean.java
@@ -7,12 +7,15 @@ import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
 
 import br.org.studio.entities.system.User;
+import br.org.studio.exceptions.DataNotFoundException;
 
 @Stateless
 @Local(UserDao.class)
 public class UserDaoBean extends GenericDaoBean implements UserDao {
 
 	private static final String EMAIL = "email";
+	private static final String ADM = "adm";
+	
 
 	@Override
 	public Boolean emailExists(String email) {
@@ -24,6 +27,13 @@ public class UserDaoBean extends GenericDaoBean implements UserDao {
 		} else {
 			return false;
 		}
+	}
+
+	@Override
+	public User findAdmin() throws DataNotFoundException {
+		Criteria query = createCriteria(User.class);
+		query.add(Restrictions.eq(ADM, Boolean.TRUE));
+		return (User) uniqueResultNotWaitingEmpty(query);
 	}
 
 }
