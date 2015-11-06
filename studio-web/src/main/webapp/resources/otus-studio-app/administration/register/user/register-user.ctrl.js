c32fa5e55444d3f9c8009bf40f838b91b40f2901
Diego Santos <santos.diegoferreira@gmail.com>
2015-11-06 11:44:36 -0200

    Integração da tela de cadastro de usuário com rest.

--- studio-web/src/main/webapp/resources/otus-studio-app/administration/register/user/register-user.ctrl.js
+++ studio-web/src/main/webapp/resources/otus-studio-app/administration/register/user/register-user.ctrl.js
@@ -1,40 +1,45 @@
-angular.module('RegisterUser', [ 'ngMaterial', 'ui.mask' ]).controller('RegisterUserCtrl', function($scope, $http, $window, $mdDialog) {
-	
-    $HTTP_POST_URL_VALIDATE = window.location.origin + '/studio/session/rest/register/validade';
-    $HTTP_POST_URL_CREATE = window.location.origin + '/studio/session/rest/register/user';
+(function() {
 
-    $scope.validateEmail = function(){
-        $http.post($HTTP_POST_URL_VALIDATE, $scope.user).then(function (response){
-            $scope.emailInUse = !response.data;
-            $scope.registerUserForm.email.$setValidity('emailInUse', response.data);
-            $scope.registerUserForm.$setValidity('emailInUse', response.data);
-        });
-    }
-    
-    $scope.register = function (user) {
-    	$http.post($HTTP_POST_URL_CREATE, user).then(function (response){
-    	    	confirmAlertToNavigate();
-    	    })
-     }
+    angular
+        .module('StudioApp', ['ngMaterial', 'ui.mask'])
+        .controller(
+            'RegisterUserCtrl',
+            function($scope, $http, $window, $mdDialog) {
 
-	$scope.matchPassword = function() {
-		if ($scope.user.password && $scope.user.passwordConfirm) {
-			if ($scope.user.password != $scope.user.passwordConfirm) {
-				$scope.registerUserForm.password.$setValidity(
-						'password', false);
-				$scope.registerUserForm.$setValidity(
-						'password', false);
-			} else {
-				$scope.registerUserForm.password.$setValidity(
-						'password', true);
-				$scope.registerUserForm.$setValidity(
-						'password', true);
-			}
-		}
-	}
+                $HTTP_POST_URL_VALIDATE = window.location.origin + '/studio/session/rest/register/validade';
+                $HTTP_POST_URL_CREATE = window.location.origin + '/studio/session/rest/register/user';
 
-	function confirmAlertRegister() {
-		alert = $mdDialog.alert().title('Informação').content('Sua liberação esta pendente de aprovação').ok('ok');
-	}
+                $scope.validateEmail = function() {
+                    $http.post($HTTP_POST_URL_VALIDATE, $scope.user).then(function(response) {
+                        $scope.emailInUse = !response.data;
+                        $scope.registerUserForm.email.$setValidity('emailInUse', response.data);
+                        $scope.registerUserForm.$setValidity('emailInUse', response.data);
+                    });
+                }
 
-});
+                $scope.register = function(user) {
+                    $http.post($HTTP_POST_URL_CREATE, user).then(function(response) {
+                        confirmAlertToNavigate();
+                    })
+                }
+
+                $scope.matchPassword = function() {
+                    if ($scope.user.password && $scope.user.passwordConfirm) {
+                        if ($scope.user.password != $scope.user.passwordConfirm) {
+                            $scope.registerUserForm.password.$setValidity('password', false);
+                            $scope.registerUserForm.$setValidity('password', false);
+                        } else {
+                            $scope.registerUserForm.password.$setValidity('password', true);
+                            $scope.registerUserForm.$setValidity('password', true);
+                        }
+                    }
+                }
+
+                function confirmAlertRegister() {
+                    alert = $mdDialog.alert().title('Informação').content('Sua liberação esta pendente de aprovação')
+                        .ok('ok');
+                }
+
+            });
+
+}());
