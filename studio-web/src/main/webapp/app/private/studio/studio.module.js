0be518c148e2708c8c125b4332d63258fef6e974
Diego Santos <santos.diegoferreira@gmail.com>
2015-12-11 16:56:36 -0200

    Adicionado método em Studio para acessar survey atualmente em edição.

--- studio-web/src/main/webapp/app/private/studio/studio.module.js
+++ studio-web/src/main/webapp/app/private/studio/studio.module.js
@@ -10,13 +10,12 @@
             self.createNewSurvey = createNewSurvey;
             self.closeSurvey = closeSurvey;
             self.loadSurvey = loadSurvey;
+            self.getCurrentSurvey = getCurrentSurvey;
 
             /* Public interface implementation */
             function createNewSurvey() {
                 var survey = SurveyLoader.newSurvey();
-                EditingService.init(survey);
-                EditingService.open();
-                console.log(EditingService.getSurvey());
+                initializeEditing(survey);
             }
 
             function closeSurvey() {
@@ -26,8 +25,16 @@
             function loadSurvey() {
                 // A persisted survey object should be load here
                 // var survey = loadSurvey();
+                // initializeEditing(survey);
+            }
+
+            function initializeEditing(survey) {
                 EditingService.init(survey);
                 EditingService.open();
+            }
+
+            function getCurrentSurvey() {
+                return EditingService.getSurvey();
             }
         }
     ]);
