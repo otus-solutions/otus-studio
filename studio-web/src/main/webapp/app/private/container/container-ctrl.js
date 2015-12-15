aac20add99486dc483dd42f77251b1cf54292513
Emanoel Vianna <emanoel.vianna@outlook.com>
2015-12-15 16:44:38 -0200

    parcial - modificações para o funcionamento da função js

--- studio-web/src/main/webapp/app/private/container/container-ctrl.js
+++ studio-web/src/main/webapp/app/private/container/container-ctrl.js
@@ -1,9 +1,11 @@
-var aux = document.querySelectorAll(".lado");
+var aux = document.querySelectorAll(".box");
 var tam = aux.length;
 
 window.onresize = sizeScreen;
 
 function sizeScreen() {
+	console.log(" Tamanho lateral: " + window.innerWidth);
+	console.log(" Tamanho altura: " + window.innerHeight);
 	if (aux) {
 		for (var i = 0; i < tam; ++i) {
 			aux[i].style.height = window.innerHeight - 20 + "px";
