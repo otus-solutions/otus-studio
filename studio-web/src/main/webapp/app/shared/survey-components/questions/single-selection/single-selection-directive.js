35b35c51bfc8be27c0ec51714e5c6c770a9504b2
Emanoel Vianna <emanoel.vianna@outlook.com>
2016-01-25 14:16:12 -0200

    QES-24 - Parcial, Refatoração do comportamento para choice

--- studio-web/src/main/webapp/app/shared/survey-components/questions/single-selection/single-selection-directive.js
+++ studio-web/src/main/webapp/app/shared/survey-components/questions/single-selection/single-selection-directive.js
@@ -1,14 +1,42 @@
 (function() {
-
-    var singleSelectionQuestion = function() {
-            var directive = {
-                templateUrl : "shared/survey-components/questions/single-selection/single-selection-question-template.html",
-                restrict : "E"
-            };
-            return directive;
-    };
-
     angular
         .module('survey.questions')
-        .directive('singleSelectionQuestion', singleSelectionQuestion);
+        .directive('singleSelectionQuestion', function() {
+            var directive = {
+                templateUrl : "shared/survey-components/questions/single-selection/single-selection-question-template.html",
+                restrict : "E",
+                link: function(scope, element, attrs) {
+                    var button = element.find("button");
+                    button.on("click", function() {
+                        var aux = element.find("md-radio-button");
+                        aux.append("<h1>Olá mundo</h1>");
+                    });
+                },
+                replace: false,
+                transclude: true,
+                scope: {
+
+                }
+            };
+            return directive;
+        });
 }());
+
+
+/**
+
+function returnNewElement() {
+    var radio = document.createElement("md-radio-button");
+    radio.setAttribute("value", "outro");
+    radio.setAttribute("editable", "");
+    radio.textContent = "outro";
+    return radio;
+}
+
+function add() {
+    var newNode = returnNewElement();
+    document.getElementById("myList").appendChild(newNode);
+}
+
+
+**/
