7ba54b2a614d61c7017261e2f43c2d4c6ffffb58
fagner <fagnerschwalm@gmail.com>
2016-01-26 18:02:34 -0200

    QES - Refatorado questão single-selection.

--- studio-web/src/main/webapp/app/shared/survey-components/questions/single-selection/single-selection-directive.js
+++ studio-web/src/main/webapp/app/shared/survey-components/questions/single-selection/single-selection-directive.js
@@ -1,38 +1,20 @@
 (function() {
+    function singleSelectionQuestion() {
+        var singleSelectionQuestionDDO = {
+            restrict: 'E',
+            scope: {},
+            templateUrl: 'shared/survey-components/questions/single-selection/single-selection-question-template.html',
+            link: function(scope, element, attrs, controller) {
+                scope.options = [1];
+                var button = element.find('button');
+                button.on('click', function() {
+                    scope.options.push(scope.options.length + 1);
+                });
+            }
+        };
+        return singleSelectionQuestionDDO;
+    }
 
-    var optionTemplatePath = 'shared/survey-components/questions/single-selection/single-selection-option-template.html';
-
-    angular
-        .module('survey.questions', [])
-        .directive('singleSelectionQuestion', ['$compile', '$templateRequest', function($compile, $templateRequest) {
-            var directive = {
-                restrict: 'E',
-                templateUrl: 'shared/survey-components/questions/single-selection/single-selection-question-template.html',
-                controller: function($scope, $element) {
-                    var self = this;
-
-                    self.loadOption = function loadOption() {
-                        $templateRequest(optionTemplatePath).then(function(html) {
-                            var compileResult = $compile(html)($scope),
-                                template = angular.element(compileResult);
-
-                            var group = angular.element(element.find('md-radio-group'));
-                            group.append(template);
-                        });
-                    };
-                },
-                link: function(scope, element, attrs, controller) {
-                    var button = element.find('button');
-                    button.on('click', function() {
-                        // var group = angular.element(element.find('md-radio-group'));
-                        // var template = '<md-radio-button value="laranja">Laranja</md-radio-button>';
-                        // var radio = angular.element('<md-radio-button flex="5">');
-                        // group.append(radio);
-                        controller.loadOption();
-                    });
-                }
-            };
-            return directive;
-        }]);
+    angular.module('survey.questions').directive('singleSelectionQuestion', singleSelectionQuestion);
 
 }());
