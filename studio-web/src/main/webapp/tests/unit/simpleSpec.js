07a750af1377b75cba4d071ea1424f57972ac3f4
fagner <fagnerschwalm@gmail.com>
2016-01-27 17:24:50 -0200

    Continuação de configuração de ambiente para testes unitários - PARCIAL.

--- studio-web/src/main/webapp/tests/unit/simpleSpec.js
+++ studio-web/src/main/webapp/tests/unit/simpleSpec.js
@@ -1,5 +1,24 @@
-describe("A suite", function() {
-    it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
+describe('ContainerController', function() { //describe your object type
+    beforeEach(module('Container')); //load module
+
+    var $controller;
+
+    beforeEach(angular.mock.inject(function(_$controller_) {
+        $controller = _$controller_;
+    }));
+
+    describe('sum', function() {
+        it('1 + 1 should equal 2', function() {
+            var $scope = {};
+            var controller = $controller('ContainerController', {
+                $scope: $scope
+            });
+            expect($scope.isOpened).toEqual(false);
+            $scope.openClose();
+            expect($scope.isOpened).toEqual(true);
+
+        });
+
     });
+
 });
