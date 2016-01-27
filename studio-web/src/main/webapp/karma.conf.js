bcaf08526f0fc6045a0ca1cbbcfe3c9c346e1560
carolinanery <carolina.nery94@gmail.com>
2016-01-27 16:15:42 -0200

    CON-14-3 configurações para teste unitário (karma-htmlfile-reporter)

--- studio-web/src/main/webapp/karma.conf.js
+++ studio-web/src/main/webapp/karma.conf.js
@@ -33,7 +33,15 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'html'],
+    
+    htmlReporter: {
+        outputFile: 'tests/unit/units.html',
+        
+        //Optional
+        pageTitle: 'Unit Tests'
+    	
+    },
 
 
     // web server port
