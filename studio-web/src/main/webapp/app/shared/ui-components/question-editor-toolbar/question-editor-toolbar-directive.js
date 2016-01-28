b94f854837fccb5d6e5e50f5b4680a4421ef2a39
Emanoel Vianna <emanoel.vianna@outlook.com>
2016-01-28 15:02:48 -0200

    FOR-25 Parcial, excluir questões

--- studio-web/src/main/webapp/app/shared/ui-components/question-editor-toolbar/question-editor-toolbar-directive.js
+++ studio-web/src/main/webapp/app/shared/ui-components/question-editor-toolbar/question-editor-toolbar-directive.js
@@ -1,10 +1,43 @@
+(function() {
+    var app = angular.module('ui.components');
+
+    app.directive("removeMe", function($rootScope) {
+
+        function remove(element) {
+            element.remove();
+        }
+
+        return {
+            link: function(scope, element, attrs) {
+                scope.removeQuestion = function() {
+                    remove(element);
+                };
+            }
+        };
+    });
+}());
+
+/**
+
+(function() {
+
 var app = angular.module('ui.components');
 app.directive("removeMe", function($rootScope) {
-    return {
-        link: function(scope, element, attrs) {
-            scope.remove = function() {
-                console.log("olá mundo");
-            };
+
+      function remove(element) {
+          element.remove();
+      }
+
+      return {
+        link:function(scope,element,attrs)
+        {
+          scope.removeQuestion = function() {
+            remove(element);
+          };
         }
-    };
+      }
 });
+
+}());
+
+**/
