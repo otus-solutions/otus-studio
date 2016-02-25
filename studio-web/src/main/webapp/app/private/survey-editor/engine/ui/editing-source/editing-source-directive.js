dbe0e28dea44ac8dd1e1b3cbe1b48e53624ff4ad
Diego Santos <santos.diegoferreira@gmail.com>
2016-02-25 14:29:58 -0300

    Separação do controller e diretiva em editing-source.

--- studio-web/src/main/webapp/app/private/survey-editor/engine/ui/editing-source/editing-source-directive.js
+++ studio-web/src/main/webapp/app/private/survey-editor/engine/ui/editing-source/editing-source-directive.js
@@ -5,61 +5,18 @@
         .module('editor.engine.ui')
         .directive('editingSource', directiveDefinition);
 
-    /*
-     * Directive initialization function
-     */
     function directiveDefinition() {
         var ddo = {
-            controller: Controller,
+            controller: 'EditingSourceController',
             link: linkImpl
         };
 
         return ddo;
     }
 
-    /*
-     * Link function implementation
-     */
     function linkImpl(scope, element, attr, controller) {
         controller.catchEditingSourceComponent();
         controller.attachTriggers();
-    }
-
-    /*
-     * Directive's controller implementation
-     */
-    Controller.$inject = ['$scope', '$element', '$attrs', 'EditingSourceService'];
-
-    function Controller($scope, $element, $attrs, EditingSourceService) {
-
-        var self = this,
-            editingSource = null;
-
-        /* Public interface */
-        self.catchEditingSourceComponent = catchEditingSourceComponent;
-        self.attachTriggers = attachTriggers;
-
-        function catchEditingSourceComponent() {
-            editingSource = EditingSourceService.createEditingSource($element, $attrs);
-
-            /*========== DEV LOG ===========*/
-            // console.info('Diretiva editing-source encontrada:');
-            // console.log('ID: ' + editingSource.id);
-            // console.log('Na tag: ' + editingSource.component.localName);
-            // console.log('Tipo de evento: ' + editingSource.type);
-            // console.log('Modelo alvo: ' + editingSource.target);
-            /*==============================*/
-        }
-
-        function attachTriggers() {
-            EditingSourceService.appendTriggersTo(editingSource);
-
-            /*========== DEV LOG ===========*/
-            // console.info('Novo objecto EditingSource:');
-            // console.log(editingSource);
-            /*==============================*/
-        }
-
     }
 
 }());
