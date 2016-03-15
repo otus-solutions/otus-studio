852d81a62788495a209dc59e2d9720ff047943fd
Diego Santos <santos.diegoferreira@gmail.com>
2016-03-15 09:45:31 -0300

    Correções em entidades do otusjs. Agora a aplicação mantém apenas a versão mais recente (completa) no IndexedDB.

--- otus-studio/private/editor/database/cross-session/cross-session-database-service.js
+++ otus-studio/private/editor/database/cross-session/cross-session-database-service.js
@@ -18,12 +18,12 @@
         function saveSurveyTemplateRevision(template, session) {
             $indexedDB.openStore('survey_template', function(store) {
                 var entry = {
-                    'template_oid': Date.now() + '-' + template.oid,
+                    'template_oid': template.oid,
                     'contributor': session.owner,
                     template: template
                 };
 
-                store.insert(entry).then(function(e) {
+                store.upsert(entry).then(function(e) {
                     console.log('Survey armazenada localmente.');
                 });
             });
--- otus-studio/shared/otusjs/model/question/time-question-factory.js
+++ otus-studio/shared/otusjs/model/question/time-question-factory.js
@@ -23,27 +23,32 @@
     function TimeQuestion(oid, prototype, UnitFactory) {
         Object.defineProperty(this, 'extends', {
             value: prototype.objectType,
-            writable: false
+            writable: false,
+            enumerable: true
         });
 
         Object.defineProperty(this, 'objectType', {
             value: 'TimeQuestion',
-            writable: false
+            writable: false,
+            enumerable: true
         });
 
         Object.defineProperty(this, 'oid', {
             value: prototype.oid,
-            writable: false
+            writable: false,
+            enumerable: true
         });
 
         Object.defineProperty(this, 'dataType', {
             value: 'LocalTime',
-            writable: false
+            writable: false,
+            enumerable: true
         });
 
         Object.defineProperty(this, 'label', {
             value: prototype.label,
-            writable: true
+            writable: true,
+            enumerable: true
         });
 
         Object.defineProperty(this, 'unit', {
@@ -52,7 +57,8 @@
                 'enUS': UnitFactory.create(),
                 'esES': UnitFactory.create()
             },
-            writable: true
+            writable: true,
+            enumerable: true
         });
     }
 
