713f1cf306041e6733add96c149ba1da6314f119
Emanoel Vianna <emanoel.vianna@outlook.com>
2016-03-17 15:48:28 -0300

    Cenários do teste survey-template-export-service

--- otus-studio/tests/unit/private/survey-editor/engine/workspace/export/survey-template-export-service-spec.js
+++ otus-studio/tests/unit/private/survey-editor/engine/workspace/export/survey-template-export-service-spec.js
@@ -1,6 +1,33 @@
 describe('Tests for survey-template-export-service', function() {
-    it("should return format correct Json", function functionName() {
-        
+
+    var Mock = {};
+
+    /* @BeforeScenario */
+    beforeEach(function() {
+        module('studio');
+
+        inject(function(_$controller_, _$injector_) {
+            /* @InjectMocks */
+            controller = _$controller_('SurveyExportService', {
+                'WorkspaceService': mockWorkspaceService(_$injector_)
+            });
+        });
     });
+
+    describe("unit test for return the json", function functionName() {
+
+        it("should return format correct Json", function functionName() {
+
+        });
+
+        it("should return object type Json", function functionName() {
+
+        });
+    });
+
+    function mockWorkspaceService($injector) {
+        Mock.WorkspaceService = $injector.get('WorkspaceService');
+        return Mock.WorkspaceService;
+    }
 
 });
