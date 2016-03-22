5dd11e76604fd509675f7fdebffeb594f4049b85
Emanoel Vianna <emanoel.vianna@outlook.com>
2016-03-22 12:11:57 -0300

    EXP-33 - parcial, teste de unidade

--- otus-studio/tests/unit/private/editor/workspace/export/survey-template-export-directive-spec.js
+++ otus-studio/tests/unit/private/editor/workspace/export/survey-template-export-directive-spec.js
@@ -1,27 +1,53 @@
 describe('surveyTemplateExportDirective', function() {
-    var compile, scope, directiveElem;
+    var scope, directiveElem;
 
     beforeEach(function() {
         module('editor.workspace');
 
         inject(function($compile, $rootScope) {
-            compile = $compile;
-            scope = $rootScope.$new();
-        });
+            elementButton = angular.element('<button survey-template-export></button>');
 
-        directiveElem = getCompiledElement();
+            scope = $rootScope.$new();
+            $compile(elementButton)(scope);
+            scope.$digest();
+        });
 
     });
 
     it('should have survey-template-export in md-button', function() {
-        var element = directiveElem.find('button[survey-template-export]');
+        var element = elementButton.find('md-button[survey-template-export]');
         expect(element.length).toBe(1);
     });
 
     function getCompiledElement() {
-        var compiledDirective = compile(angular.element('<button id="test" survey-template-export></button>'))(scope);
+        var compiledDirective = compile(angular.element('<button survey-template-export></button>'))(scope);
         scope.$digest();
         return compiledDirective;
     }
+
+});
+
+
+
+
+describe('surveyTemplateExportDirective:?', function() {
+	beforeEach(module('editor.workspace'));
+
+	var element, scope;
+
+	beforeEach(module('views/templates/albums.html'));
+
+	beforeEach(inject(function($rootScope, $compile) {
+		element = angular.element('<button survey-template-export></button>');
+
+		scope = $rootScope;
+		$compile(element)(scope);
+		scope.$digest();
+	}));
+
+    it('should have survey-template-export in md-button', function() {
+        var element = directiveElem.find('md-button[survey-template-export]');
+        expect(element.length).toBe(1);
+    });
 
 });
