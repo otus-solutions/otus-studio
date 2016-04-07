81e7e0c6f96ea15fcbd663ec1b8ce1ee1699be6c
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2016-04-07 20:01:59 -0300

    GULP para Gerencia do Browser Sync

--- otus-studio/gulpfile.js
+++ otus-studio/gulpfile.js
@@ -1,19 +1,27 @@
 (function() {
 
     var gulp = require('gulp');
+    var browserSync = require('browser-sync').create();
 
     /* Task registry */
     gulp.task('default', defaultTask);
 
-    /* Task implementation */
-    // function defaultTask() {
-    //     return gulp.src('app/*/*.html')
-    //         .pipe(useref())
-    //         .pipe(gulp.dest('dist'));
-    // }
-
     function defaultTask() {
         console.log('Do stuff here...');
     }
+
+    gulp.task('browser-sync', function() {
+        browserSync.init({
+            server: {
+                baseDir: "../"
+            }
+        });
+
+        gulp.watch([
+            'app/**/*.html',
+            'app/**/*.js',
+            'app/**/*.css'
+        ]).on('change', browserSync.reload);
+    });
 
 }());
