c06b61f99037a6fc3661498c1bfd57194fa7927c
Diego Santos <santos.diegoferreira@gmail.com>
2016-05-03 16:59:48 -0300

    Integração com dev.

--- otus-studio/app/preview/survey-navigation/survey-navigation-preview-directive.js
+++ otus-studio/app/preview/survey-navigation/survey-navigation-preview-directive.js
@@ -1,22 +1,25 @@
 (function() {
     'use strict';
 
-    var navigationObject=[{origin:"LAPA1",destinations:[{to:"LAPA2"},{to:"LAPA4"}]},{origin:"LAPA2",destinations:[{to:"LAPA3"}]},{origin:"LAPA3",destinations:[{to:"LAPA4"}]},{origin:"LAPA4",destinations:[{to:"LAPA5"}]},{origin:"LAPA5",destinations:[{to:"LAPA6"}]},{},{origin:"LAPA6",destinations:[{to:"LAPA7"}]},{origin:"LAPA7"}];
     var DOM_LOCATION_NAVEGATION_GRAPH = '#survey-navigation-graph > svg:nth-child(1)';
 
     angular
         .module('preview.navigation')
         .directive('surveyNavigationPreviewGenerator', surveyNavigationPreviewGenerator);
 
-    surveyNavigationPreviewGenerator.$inject = ['NavigationPreviewService'];
+    surveyNavigationPreviewGenerator.$inject = [
+        'NavigationPreviewService',
+        'WorkspaceService'
+    ];
 
-    function surveyNavigationPreviewGenerator(NavigationPreviewService) {
+    function surveyNavigationPreviewGenerator(NavigationPreviewService, WorkspaceService) {
 
         function link(scope, element, attrs) {
             element.on('click', function(){
                 if(element.find(DOM_LOCATION_NAVEGATION_GRAPH)){
                     $(DOM_LOCATION_NAVEGATION_GRAPH).remove();
                 }
+                var navigationObject = WorkspaceService.getSurvey().navigationList;
                 var createdGraph = NavigationPreviewService.createGraph(navigationObject);
                 NavigationPreviewService.renderGraph(createdGraph);
             });
