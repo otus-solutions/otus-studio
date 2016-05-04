fbd4be49d8636768cd8766e5515a959793237722
Emanoel Vianna <emanoel.vianna@outlook.com>
2016-05-04 15:50:19 -0300

    QES-50 - modificação de pasta

--- otus-studio/tests/unit/shared/otusjs/model/question/phone/phone-question-widget-factory-spec.js
+++ otus-studio/tests/unit/shared/otusjs/model/question/phone/phone-question-widget-factory-spec.js
@@ -1,12 +1,13 @@
 describe('PhoneQuestionWidgetFactory', function functionName() {
     var phoneQuestionWidgetFactory;
     var phoneQuestionWidget;
+    var phone;
 
     beforeEach(function() {
         phoneQuestionWidgetFactory = jasmine.createSpyObj('phoneQuestionWidgetFactory', ['create']);
-        phoneQuestionWidgetFactory.create();
-        phoneQuestionWidget = jasmine.createSpy('phoneQuestionWidget');
-        phoneQuestionWidget();
+        phone = phoneQuestionWidgetFactory.create();
+        // phoneQuestionWidget = jasmine.createSpy('phoneQuestionWidget');
+        // phone = new phoneQuestionWidget();
     });
 
     describe('group of test for PhoneQuestionWidgetFactory method', function functionName() {
@@ -20,7 +21,7 @@ describe('PhoneQuestionWidgetFactory', function functionName() {
 
     describe('group does test for PhoneQuestionWidget method', function functionName() {
         xit('should construct object correct', function functionName() {
-            expect(phoneQuestionWidget.name).toEqual('PhoneQuestion');
+            expect(phone.name).toEqual('PhoneQuestion');
         });
     });
 
