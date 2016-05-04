462f47576b3f8eb183e74841a3659146756b4c2a
carolinanery <carolina.nery94@gmail.com>
2016-05-04 16:39:52 -0300

    QES-48 teste de aceitação decimalQuestionWidgetFactory (incompleto)

--- otus-studio/tests/unit/app/editor/ui/question/decimal/decimal-question-widget-factory-spec.js
+++ otus-studio/tests/unit/app/editor/ui/question/decimal/decimal-question-widget-factory-spec.js
@@ -1,17 +1,37 @@
 describe('DecimalQuestionWidgetFactory', function(){
     var Mock = {},
-        NAME = 'NAME',
-        QUESTION_ID = 'QUESTION_ID';
+        QUESTIONWIDGET = 'QUESTIONWIDGET';
 
     beforeEach(function() {
-        module('editor.ui');
+        module('studio');
+        mockQuestion();
 
-        inject(function(_$injector_) {
-            factory = _$injector_.get('DecimalQuestionWidgetFactory', {
-                'DecimalQuestionFactory': mockDecimalQuestionFactory(_$injector_)
-            });
+        inject(function(_$injector_){
+            factory = _$injector_.get('QuestionWidgetFactory');
         });
 
-        decimalQuestionWidget = factory.create(NAME, QUESTION_ID);
+        decimalQuestionWidget = factory.create(Mock.question);
     });
+
+    describe('DecimalQuestionWidgetFactory', function() {
+        it('should return a object with correct name', function() {
+            expect(decimalQuestionWidget.name).toEqual('DecimalQuestion');
+        });
+
+        xit('should return a object parentWidget', function() {
+            expect(decimalQuestionWidget.parentWidget).toBe(QUESTIONWIDGET);
+        });
+
+        it('should return a object template equal to decimal-question', function() {
+            expect(decimalQuestionWidget.template).toEqual('<decimal-question></decimal-question>');
+        });
+
+    });
+
+    function mockQuestion() {
+        Mock.question = {
+            objectType : 'DecimalQuestion'
+        };
+    }
+
 });
