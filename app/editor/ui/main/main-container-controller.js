284a54c44dd4de6e9711636a9fff6cec78a992cb
brenoscheffer <brenoscheffer@gmail.com>
2016-06-09 17:59:25 -0300

    LAY-29 Menu Edição - Finalizado

--- app/editor/ui/main/main-container-controller.js
+++ app/editor/ui/main/main-container-controller.js
@@ -7,37 +7,29 @@
     MainContainerController.$inject = [
         '$scope',
         'MainContainerContentService',
-        'WorkspaceService',
         'UiBindingService',
         '$mdBottomSheet'
     ];
 
-    function MainContainerController($scope, MainContainerContentService, WorkspaceService, UiBindingService, $mdBottomSheet) {
+    function MainContainerController($scope, MainContainerContentService, UiBindingService, $mdBottomSheet) {
         var self = this;
+
         self.showQuestionsMenu = showQuestionsMenu;
 
         init();
 
         function init() {
-            self.showQuestionProperties = false;
             MainContainerContentService.init(self);
-          //  self.showQuestionsMenu();
-            console.log("chamei");
         }
-
         UiBindingService.setScope($scope);
 
         function showQuestionsMenu() {
             $mdBottomSheet.show({
                 templateUrl: 'app/editor/ui/survey-item-palette/bottom-sheet.html',
-                //clickOutsideToClose: true,
-                disableBackdrop: true,
+                //disableBackdrop: true,
                 disableParentScroll: false
             });
-
-            //console.log("tchururu");
         }
     }
-
 
 }());
