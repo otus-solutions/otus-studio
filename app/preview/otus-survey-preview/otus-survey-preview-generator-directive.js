3f44e084524ffac1fef3150514add0cc076cdc6a
fagner <fagnerschwalm@gmail.com>
2016-07-14 14:13:20 -0300

    FOR-60 - Refatorado diretiva. Destrói o scope quando o elemento é removido do DOM.

--- app/preview/otus-survey-preview/otus-survey-preview-generator-directive.js
+++ app/preview/otus-survey-preview/otus-survey-preview-generator-directive.js
@@ -5,32 +5,40 @@
         .module('preview')
         .directive('otusSurveyPreviewGenerator', otusSurveyPreviewGenerator);
 
-    otusSurveyPreviewGenerator.$inject = ['$compile', 'WorkspaceService'];
-
-    function otusSurveyPreviewGenerator($compile, WorkspaceService) {
-
-        function linkFunc($scope, $element) {
-            $element.on('click', function() {
-                var newScope = $scope.$new(true);
-                var otusSheet = $('otus-sheet');
-                if(otusSheet) {
-                    otusSheet.remove();
-                }
-                var surveyTemplateJson = WorkspaceService.getSurvey().toJson();
-                var surveyTemplateObject = JSON.parse(surveyTemplateJson);
-                newScope.surveyTemplate = surveyTemplateObject;
-                var component = angular.element('<otus-sheet md-theme="layoutTheme" survey-template="surveyTemplate" layout="column" flex="80"></otus-sheet>');
-                var content = $compile(component)(newScope);
-                $('#survey-preview').append(content);
-            });
-        }
-
+    function otusSurveyPreviewGenerator() {
         var ddo = {
             restrict: 'A',
-            link: linkFunc
+            controller: Controller
         };
-
         return ddo;
+    }
+
+    Controller.$inject = ['$scope', '$element', '$compile', 'WorkspaceService'];
+
+    function Controller($scope, $element, $compile, WorkspaceService) {
+        var OTUS_SHEET_COMPONENT = '<otus-sheet md-theme="layoutTheme" survey-template="surveyTemplate" layout="column" flex="80"></otus-sheet>';
+        var _newScope;
+
+        $element.on('click', function() {
+            var otusSheetDOMElement = $('otus-sheet');
+
+            if (otusSheetDOMElement[0]) {
+                otusSheetDOMElement.remove();
+                _newScope.$destroy();
+            }
+            _generateOtusPreview();
+        });
+
+        function _generateOtusPreview() {
+            _newScope = $scope.$new(true);
+            _newScope.surveyTemplate = _getSurveyTemplateObject();
+            var content = $compile(OTUS_SHEET_COMPONENT)(_newScope);
+            $('#survey-preview').append(content);
+        }
+
+        function _getSurveyTemplateObject() {
+            return JSON.parse(WorkspaceService.getSurvey().toJson());
+        }
     }
 
 })();
