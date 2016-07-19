8245424b0209f182de0b6a315173a073a5012687
jenkins <jenkins@ccem.ufrgs.br>
2016-07-19 11:03:17 -0300

    JENKINS - Update Versão

--- dist/otus-studio.js
+++ dist/otus-studio.js
@@ -861,6 +861,24 @@
     'use strict';
 
     angular
+        .module('studio.dashboard')
+        .directive('otusToolbar', otusToolbar);
+
+    function otusToolbar() {
+        var ddo = {
+            templateUrl: 'app/dashboard/menu/toolbar/menu-toolbar.html',
+            retrict: 'E'
+        };
+
+        return ddo;
+    }
+
+}());
+
+(function() {
+    'use strict';
+
+    angular
         .module('surveyTemplates')
         .service('SelectedSurveyTemplatesManagementService', SelectedSurveyTemplatesManagementService);
 
