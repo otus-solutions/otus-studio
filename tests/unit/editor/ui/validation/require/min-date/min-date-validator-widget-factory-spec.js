9ade0b97a24b3f7f8fca37b46774e426e876d5c8
brenoscheffer <brenoscheffer@gmail.com>
2016-07-27 15:12:02 -0300

    QES-63 Estrutura de testes dos validadores

---
+++ tests/unit/editor/ui/validation/require/min-date/min-date-validator-widget-factory-spec.js
@@ -0,0 +1,87 @@
+xdescribe('MinDateValidatorWidgetFactory', function() {
+    var Mock = {};
+    var factory;
+    var whoAmI;
+
+    beforeEach(function() {
+        module('studio');
+
+        mockElement();
+
+        inject(function(_$injector_) {
+            mockWidgetScope(_$injector_);
+            factory = _$injector_.get('MinDateValidatorWidgetFactory');
+        });
+
+        widget = factory.create(Mock.scope, Mock.element);
+    });
+
+    describe('Start a MinDate Factory Object', function() {
+        it('should return a MinDate Validator Object', function() {
+            pending();
+        });
+
+        it('should start the data field with model value (default: current date)  ', function() {
+            expect(widget.data).toBeDefined();
+            expect(widget.data.toLocaleDateString()).toBe(dateModel);
+        });
+    });
+
+
+    describe('updates on data', function() {
+        it('should model data value be equal to self value', function() {
+          console.log(Mock.question.fillingRules.options['minDate'].data.reference);
+            expect(Mock.question.fillingRules.options['minDate'].data.reference).toBe(widget.data);
+        });
+
+        it('should call updateFillingRules from parente widget', function() {
+            spyOn(Mock.parentWidget, 'updateFillingRules');
+
+            widget.updateData();
+
+            expect(Mock.parentWidget.updateFillingRules).toHaveBeenCalled();
+        });
+
+
+    });
+
+
+    function mockElement() {
+        Mock.element = {};
+    }
+
+    function mockWidgetScope($injector) {
+        Mock.scope = {
+            class: '',
+            $parent: {
+                widget: mockParentWidget($injector)
+            }
+        };
+        return Mock.scope;
+    }
+    //
+    function mockParentWidget($injector) {
+        mockQuestion($injector);
+
+        Mock.parentWidget = {
+            getItem: function() {
+                return Mock.question;
+            },
+            updateFillingRules: function() {}
+        };
+
+        return Mock.parentWidget;
+    }
+
+    function mockQuestion($injector) {
+        Mock.question = $injector.get('SurveyItemFactory').create('IntegerQuestion', 'Q1');
+        Mock.question.fillingRules.options['minDate'] = $injector.get('RulesFactory').create('minDate');
+        return Mock.question;
+    }
+
+    function mockAdd($injector) {
+        Mock.add = $injector.get('FillingRulesEditorWidgetFactory').create();
+
+    }
+
+});
