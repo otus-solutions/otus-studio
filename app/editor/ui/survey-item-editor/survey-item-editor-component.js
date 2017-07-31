9d56abaac9c0dd3ddf6c18cf7076211d2a5a4f48
Fagner <fagnerschwalm@gmail.com>
2017-07-31 17:03:24 -0300

    Remoção de widget factories - Parcial.

--- app/editor/ui/survey-item-editor/survey-item-editor-component.js
+++ app/editor/ui/survey-item-editor/survey-item-editor-component.js
@@ -15,15 +15,35 @@
     'SurveyItemEditorWidgetFactory',
     'UUIDService',
     '$scope',
-    '$element'
+    '$element',
+    'RemoveSurveyItemEventFactory'
   ];
 
-  function Controller(SurveyItemEditorWidgetFactory, UUIDService, $scope, $element) {
+  function Controller(SurveyItemEditorWidgetFactory, UUIDService, $scope, $element, RemoveSurveyItemEventFactory) {
     var self = this;
+    // lifecycle hooks
+    self.$onInit = onInit;
+    // public methods
+    self.getItem = getItem;
+    self.deleteSurveyItem = deleteSurveyItem;
+    self.getQuestionId = getQuestionId;
 
-    self.$onInit = function onInit() {
-      $scope.uuid = UUIDService.generateUUID();
-      $scope.widget = SurveyItemEditorWidgetFactory.create($scope, $element, self.item);
+    function getItem() {
+      return self.item;
+    }
+
+    function getQuestionId() {
+      return self.getItem().templateID;
+    }
+
+    function deleteSurveyItem() {
+      RemoveSurveyItemEventFactory.create().execute(self.item);
+    }
+
+
+    function onInit() {
+      // $scope.uuid = UUIDService.generateUUID();
+      // $scope.widget = SurveyItemEditorWidgetFactory.create($scope, $element, self.item);
     }
 
   }
