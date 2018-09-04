7cc5615b4f66c09badbfee01e8e8ea6e3c69210c
Adonis Garcia <adonis.garcia.adg@gmail.com>
2018-09-04 11:34:03 -0300

    OS-35 - Refaturado e função removida

--- app/editor/ui/base/text-editor/text-editor-widget-factory.js
+++ app/editor/ui/base/text-editor/text-editor-widget-factory.js
@@ -1,90 +1,86 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('editor.ui')
-        .factory('OtusTextEditorWidgetFactory', OtusTextEditorWidgetFactory);
+  angular
+    .module('editor.ui')
+    .factory('OtusTextEditorWidgetFactory', OtusTextEditorWidgetFactory);
 
-    OtusTextEditorWidgetFactory.$inject = [
-        'UpdateQuestionEventFactory'
-    ];
+  OtusTextEditorWidgetFactory.$inject = [
+    'UpdateQuestionEventFactory'
+  ];
 
-    function OtusTextEditorWidgetFactory(UpdateQuestionEventFactory) {
-        var self = this;
+  function OtusTextEditorWidgetFactory(UpdateQuestionEventFactory) {
+    var self = this;
 
-        self.create = create;
+    self.create = create;
 
-        function create(scope, element) {
-            return new OtusTextEditorWidget(scope, element, UpdateQuestionEventFactory);
-        }
-        return self;
+    function create(scope, element) {
+      return new OtusTextEditorWidget(scope, element, UpdateQuestionEventFactory);
+    }
+    return self;
+  }
+
+  function OtusTextEditorWidget(scope, element, UpdateQuestionEventFactory) {
+    var self = this;
+
+    self.input = angular.element(element.children()[0]);
+    self.ngModel = scope.ngModel;
+    self.placeholder = scope.placeholder;
+
+    /* Public methods */
+    self.getClassName = getClassName;
+    self.getUUID = getUUID;
+    self.getElement = getElement;
+    self.getParent = getParent;
+    self.getItem = getItem;
+    self.getLabel = getLabel;
+
+    _init();
+
+    function _init() {
+      if (self.ngModel) {
+        _loadLabel();
+      }
     }
 
-    function OtusTextEditorWidget(scope, element, UpdateQuestionEventFactory) {
-        var self = this;
-
-        self.input = angular.element(element.children()[0]);
-        self.ngModel = scope.ngModel;
-        self.placeholder = scope.placeholder;
-
-        /* Public methods */
-        self.getClassName = getClassName;
-        self.getUUID = getUUID;
-        self.getElement = getElement;
-        self.getParent = getParent;
-        self.getItem = getItem;
-        self.getLabel = getLabel;
-
-        _init();
-
-        function _init() {
-            if (self.ngModel) {
-                _loadLabel();
-            }
-        }
-
-        function getClassName() {
-            return 'OtusTextEditorWidget';
-        }
-
-        function getUUID() {
-            return scope.uuid;
-        }
-
-        function getLabel() {
-            return self.ngModel.ptBR.formattedText;
-        }
-
-        function getElement() {
-            return element;
-        }
-
-        function getParent() {
-            return scope.$parent.widget;
-        }
-
-        function getItem() {
-            return getParent().getItem();
-        }
-
-
-        element.on('focusout', function(event) {
-            _saveLabel();
-            UpdateQuestionEventFactory.create().execute(self);
-        });
-
-        function _saveLabel() {
-            self.ngModel.ptBR.formattedText = _removeSpecialCharacters(event.target.innerHTML);
-            self.ngModel.ptBR.plainText = event.target.innerText;
-        }
-
-        function _removeSpecialCharacters(value) {
-            return value.replace(/"/g, '\'');
-        }
-
-        function _loadLabel() {
-            self.getElement().children()[0].innerHTML = self.getLabel();
-        }
+    function getClassName() {
+      return 'OtusTextEditorWidget';
     }
+
+    function getUUID() {
+      return scope.uuid;
+    }
+
+    function getLabel() {
+      return self.ngModel.ptBR.formattedText;
+    }
+
+    function getElement() {
+      return element;
+    }
+
+    function getParent() {
+      return scope.$parent.widget;
+    }
+
+    function getItem() {
+      return getParent().getItem();
+    }
+
+
+    element.on('focusout', function(event) {
+      _saveLabel();
+      UpdateQuestionEventFactory.create().execute(self);
+    });
+
+    function _saveLabel() {
+      self.ngModel.ptBR.formattedText = event.target.innerHTML;
+      self.ngModel.ptBR.plainText = event.target.innerText;
+    }
+
+    function _loadLabel() {
+      self.getElement().children()[0].innerHTML = self.getLabel();
+    }
+  }
 
 }());
