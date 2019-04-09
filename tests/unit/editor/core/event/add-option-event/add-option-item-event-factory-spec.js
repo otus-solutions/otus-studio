6aa0dee846b918ed00b215f2b2502a0af9e6297f
Adonis Garcia <adonis.garcia.adg@gmail.com>
2019-04-09 18:01:22 -0300

    teste unidade add-option-item-event-factory-spec.js

--- tests/unit/editor/core/event/add-option-event/add-option-item-event-factory-spec.js
+++ tests/unit/editor/core/event/add-option-event/add-option-item-event-factory-spec.js
@@ -1,15 +1,61 @@
-describe('AddOptionItemEventFactory', function() {
-  var Mock = {};
+describe('AddOptionItemEventFactory suite', function () {
+
   var factory, event;
-  var mockWorkService;
-  var question;
+  var Mock = {};
 
   beforeEach(function() {
-    angular.mock.module('studio');
+
+    mockWorkspace();
+
+    angular.mock.module('studio',function ($provide) {
+      $provide.value('WorkspaceService',Mock.workspace);
+    });
+
+    angular.mock.inject(function(_$injector_) {
+
+      mockTimeQuestionFactory(_$injector_);
+
+      factory = _$injector_.get('AddOptionItemEventFactory');
+    });
+
+    spyOn(factory, 'create').and.callThrough();
+
   });
 
-  beforeEach(function() {
-    mockWorkService = {
+  it('factoryExistence check', function () {
+    expect(factory).toBeDefined();
+  });
+  
+  describe('factoryInstance', function() {
+    beforeEach(function() {
+      event = factory.create();
+      event.execute(Mock.question, jasmine.any(String), jasmine.any(String));
+    });
+
+    it('factoryMethodExistence check', function () {
+      expect(factory.create).toBeDefined();
+    });
+
+    it('create method should ', function () {    
+      expect(factory.create).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return an instance of AddOptionItemEvent', function() {
+      expect(event.constructor.name).toBe('AddOptionItemEvent');
+    });
+    
+    it('should return an object with execute method', function () {
+      expect(event.execute).toBeDefined();
+    })
+  });
+  
+  function mockTimeQuestionFactory($injector) {
+    Mock.TimeQuestionFactory = $injector.get('TimeQuestionFactory');
+    Mock.question = Mock.TimeQuestionFactory.create('TimeQuestion', jasmine.any(String));
+  }
+
+  function mockWorkspace() {
+    Mock.workspace = {
       workspace: {
         isdb: {
           userEdits: {
@@ -20,37 +66,9 @@ describe('AddOptionItemEventFactory', function() {
         }
       },
       saveWork: function() {
-          return true;
-        }
+        return true;
+      }
     };
-    angular.mock.module(function($provide) {
-      $provide.value('WorkspaceService', mockWorkService);
-    });
-
-  })
-
-  beforeEach(function() {
-    angular.mock.inject(function(_$injector_) {
-      factory = _$injector_.get('AddOptionItemEventFactory');
-      mockTimeQuestionFactory(_$injector_);
-      event = factory.create();
-      event.execute(question, jasmine.any(String), jasmine.any(String));
-    });
-  });
-
-  describe('create method', function() {
-    it('should return an instance of AddOptionItemEvent', function() {
-      expect(event.constructor.name).toBe('AddOptionItemEvent');
-    });
-
-    it('should return an object with execute method', function() {
-      expect(event.execute).toBeDefined();
-    });
-  });
-
-
-  function mockTimeQuestionFactory($injector) {
-    Mock.TimeQuestionFactory = $injector.get('TimeQuestionFactory');
-    question = Mock.TimeQuestionFactory.create('TimeQuestion', jasmine.any(String));
   }
+
 });
