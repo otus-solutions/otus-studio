6eaf6221207667e5728f39e0c84c6b9ad73b2aa2
Adonis Garcia <adonis.garcia.adg@gmail.com>
2019-04-11 15:01:51 -0300

    teste unidade add-data-source-event-factory-spec.js

---
+++ tests/unit/editor/core/event/data-source/add-data-source-event-factory-spec.js
@@ -0,0 +1,69 @@
+describe('AddDataSourceEventFactory Suite', function () {
+
+  var factory;
+  var Mock = {};
+  var Injections = [];
+  var ITEM_TYPE = 'AutocompleteQuestion';
+  var NAME = 'teste';
+
+  beforeEach(function () {
+    angular.mock.module('studio');
+
+    angular.mock.inject(function (_$injector_) {
+
+      Injections.WorkspaceService = _$injector_.get('WorkspaceService');
+      Injections.WorkspaceService.workspace = mockWorkspace();
+      Mock.item = _$injector_.get('SurveyItemFactory').create('AutocompleteQuestion', 'Q1');
+
+      factory = _$injector_.get('AddDataSourceEventFactory', Injections);
+
+      mockSurvey();
+      spyOn(Injections.WorkspaceService, "getSurvey").and.returnValue(Mock.survey);
+      spyOn(Injections.WorkspaceService, "saveWork");
+    });
+  });
+
+  it('factoryExistence check', function () {
+    expect(factory).toBeDefined();
+  });
+
+  it('factoryMethodsExistence check', function () {
+    expect(factory.create).toBeDefined();
+  });
+
+  it('create_method_should_instantiate_AddFillingRulesEvent', function () {
+    expect(factory.create().execute).toBeDefined();
+  });
+
+  it('Instance_of_AddFillingRulesEventFactory_should_evoke_internalMethods', function () {
+    factory.create().execute(Mock.item,NAME);
+    expect(Injections.WorkspaceService.workspace.isdb.userEdits.store).toHaveBeenCalledTimes(1);
+    expect(Injections.WorkspaceService.saveWork).toHaveBeenCalledTimes(1);
+  });
+
+  function mockWorkspace() {
+    return Mock.workspace = {
+      isdb: {
+        userEdits: {
+          store: jasmine.createSpy()
+        }
+      },
+      project: {
+        survey: jasmine.createSpy()
+      },
+      sessions: {
+        workspaceOwner: jasmine.createSpy()
+      }
+    }
+  }
+
+  function mockSurvey() {
+    Mock.performBind = {
+      performBind: jasmine.createSpy("performBind").and.returnValue(Mock.item.templateID)
+    };
+
+    Mock.survey = {
+      getDataSource: jasmine.createSpy("getDataSource").and.returnValue(Mock.performBind)
+    }
+  }
+});
