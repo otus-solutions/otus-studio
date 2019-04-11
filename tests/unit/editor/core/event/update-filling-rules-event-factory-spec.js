f4c1bea2e6943986ceed5186f007fbb073af1cef
Fabiano Ramires <fdrtec@gmail.com>
2019-04-11 12:53:30 -0300

    OS-revisionTests-i134 :  4 unitTests para factory de updateFillingRulesEvent

---
+++ tests/unit/editor/core/event/update-filling-rules-event-factory-spec.js
@@ -0,0 +1,53 @@
+describe('UpdateFillingRulesEventFactory_Suite', function () {
+
+  var factory;
+  var Mock = {};
+  var Injections = [];
+
+  beforeEach(function () {
+    angular.mock.module('studio');
+
+    angular.mock.inject(function (_$injector_) {
+
+      Injections.WorkspaceService = _$injector_.get('WorkspaceService');
+      Injections.WorkspaceService.workspace = mockWorkspace();
+
+      factory = _$injector_.get('UpdateFillingRulesEventFactory', Injections);
+      spyOn(Injections.WorkspaceService, "saveWork");
+    });
+  });
+
+  it('factoryExistence check', function () {
+    expect(factory).toBeDefined();
+  });
+
+  it('factoryMethodsExistence check', function () {
+  expect(factory.create).toBeDefined();
+  });
+
+  it('create_method_should_instantiate_UpdateFillingRulesEvent', function () {
+    expect(factory.create().execute).toBeDefined();
+  });
+
+  it('execute_method_should_evoke_internalMethods_of_instance_UpdateFillingRulesEvent', function () {
+    factory.create().execute();
+    expect(Injections.WorkspaceService.workspace.isdb.userEdits.store).toHaveBeenCalledTimes(1);
+    expect(Injections.WorkspaceService.saveWork).toHaveBeenCalledTimes(1);
+  });
+
+  function mockWorkspace() {
+    return Mock.workspace = {
+      isdb: {
+        userEdits: {
+          store: jasmine.createSpy()
+        }
+      },
+      project: {
+        survey: jasmine.createSpy()
+      },
+      sessions: {
+        workspaceOwner: jasmine.createSpy()
+      }
+    }
+  };
+});
