84ced688aa148bc8a7541cae4bd33f503b535bde
Fabiano Ramires <fdrtec@gmail.com>
2019-08-07 13:46:35 -0300

    OA-144 select da dialog listando os acronyms: Marco do fim da tentativa com dialog

--- app/editor/ui/survey-item-editor/survey-item-group/survey-item-group-controller.js
+++ app/editor/ui/survey-item-editor/survey-item-group/survey-item-group-controller.js
@@ -7,48 +7,15 @@
 
   function DialogController(data, WorkspaceService) {
     var vm = this;
+    vm.selectedQuestion = 1;
     //console.log(WorkspaceService.getSurvey().getItems());
     console.log(data);
 
     vm.enabledQuestions = _questionValidator(WorkspaceService) || [];
     console.log(vm.enabledQuestions);
 
-    // vm.selectedQuestionNeighbor = 0;
-    // vm.selectedCriteria = 1;
-    // vm.item =  data.item;
-    // vm.position = data.position;
-    // vm.TEXT = data.text;
-    // vm.questions =  angular.copy(data.questions) || [];
-    //
-    // vm.questions.forEach(function (question, index) {
-    //   question.position = index;
-    // });
-    // vm.questions = vm.questions.filter(function (question) {
-    //   return question.templateID != data.item.templateID;
-    // });
-    //
-    // _buildCriteria();
-    //
-    // vm.BUTTONS = data.buttons || [];
-    // vm.cancel = data.cancel;
-    //
-    // vm.isBeginQuestion = function () {
-    //   vm.selectedQuestionNeighbor = vm.selectedCriteria > 0 ? vm.selectedQuestionNeighbor : -1;
-    // };
-    //
-    // function _buildCriteria() {
-    //   if (vm.position == 1){
-    //     vm.criterias = [
-    //       {label:"Após a", value: 1}
-    //     ];
-    //   } else {
-    //     vm.criterias = [
-    //       {label:"Início", value: 0},
-    //       {label:"Após a", value: 1}
-    //     ];
-    //   }
-
-    //}
+    vm.BUTTONS = data.buttons || [];
+    vm.cancel = data.cancel;
   }
 
   function _questionValidator(WorkspaceService){
