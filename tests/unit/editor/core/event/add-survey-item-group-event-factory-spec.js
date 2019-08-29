345f7cf359909d4b4a04e5090a8bbc5d12a20675
Fabiano Ramires <fdrtec@gmail.com>
2019-08-29 08:56:39 -0300

    OS-44 3 unitTests para a factory de add-survey-item-group-event

--- tests/unit/editor/core/event/add-survey-item-group-event-factory-spec.js
+++ tests/unit/editor/core/event/add-survey-item-group-event-factory-spec.js
@@ -1,11 +1,10 @@
 describe('AddSurveyItemGroupEventFactory_UnitTests_Suite', function () {
 
   var factory;
-  var Injections= [];
-  var Mock = {};
+  var Injections = [];
 
   beforeEach(function () {
-    angular.mock.module('editor.core');
+    angular.mock.module('studio');
     angular.mock.inject(function ($injector) {
       Injections.WorkspaceService = $injector.get('WorkspaceService');
       factory = $injector.get('AddSurveyItemGroupEventFactory', Injections);
@@ -13,8 +12,15 @@ describe('AddSurveyItemGroupEventFactory_UnitTests_Suite', function () {
   });
 
   it('factoryExistenceCheck', function () {
-    console.log(factory);
-
+    expect(factory).toBeDefined();
   });
 
+  it('factoryMethodsExistence_check', function () {
+    expect(factory.create).toBeDefined();
+  });
+
+  it('createMethod_should_create_an_instance_of_ AddSurveyItemGroupEvent', function () {
+    let addSurveyItemGroupEvent = factory.create();
+    expect(typeof addSurveyItemGroupEvent.execute).toBe('function');
+  });
 });
