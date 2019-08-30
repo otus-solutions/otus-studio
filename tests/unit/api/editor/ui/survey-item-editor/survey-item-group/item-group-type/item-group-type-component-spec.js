5a352dd83a949b72157e05873f8ac173b209b140
Fabiano Ramires <fdrtec@gmail.com>
2019-08-30 11:33:35 -0300

    OS-44 unitTest para existencia do controle de itemGroupTypeComponent

--- tests/unit/api/editor/ui/survey-item-editor/survey-item-group/item-group-type/item-group-type-component-spec.js
+++ tests/unit/api/editor/ui/survey-item-editor/survey-item-group/item-group-type/item-group-type-component-spec.js
@@ -1,30 +1,15 @@
-// describe('itemGroupTypeComponent_UnitTests_Suite', function () {
-//   var component;
-//   var Injections = [];
-//   var Mock = {};
-//
-//   beforeEach(function () {
-//      angular.mock.module('studio');
-//      angular.mock.inject(function ($componentController) {
-  //     Injections.ApplicationStateService = $injector.get("otusjs.application.state.ApplicationStateService");
-  //     Injections.SignupService = $injector.get("otusjs.user.access.service.SignupService");
-  //     Injections.LoadingScreenService = $injector.get("otusjs.deploy.LoadingScreenService");
-  //     Injections.$q = $injector.get('$q'); //controlar promises (
-  //     component = $componentController('itemGroupType', null, {});
-  //
-  //     Mock.user = Test.utils.data.userSignup; //pegar contexto(json com dados)
-  //     spyOn(Injections.SignupService, "executeSignup", Mock.user).and.callThrough();
- //    })
- // });
-  //
-  // it('controllerExistence check ', function () {
-  //   console.log(component)
-  //   //expect(ctrl).toBeDefined();
-  // });
-  //
-  // it('controllerMethodsExistence check', function () {
-  //   expect(ctrl.signup).toBeDefined();
-  //   expect(ctrl.back).toBeDefined();
-  //   expect(ctrl.agree).toBeDefined();
-  //   expect(ctrl.resetEmailValidation).toBeDefined();
-  // });
+describe('itemGroupTypeComponent_UnitTests_Suite', function () {
+  let ctrl;
+
+  beforeEach(function () {
+    angular.mock.module('studio');
+    angular.mock.inject(function ($controller) {
+      ctrl = $controller('itemGroupTypeCtrl');
+    })
+  });
+
+  it('componentExistence check ', function () {
+    expect(ctrl).toBeDefined();
+  });
+});
+
