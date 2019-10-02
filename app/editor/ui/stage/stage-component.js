ba61e6d08934ef1d18fcb143e3e2c594c0c4a4aa
Adriano <desvacc@gmail.com>
2019-10-02 17:48:31 -0300

    OSFE-1 hide header on scroll down

--- app/editor/ui/stage/stage-component.js
+++ app/editor/ui/stage/stage-component.js
@@ -5,9 +5,45 @@
     .module('editor.ui')
     .component('otusStage', {
       templateUrl: 'app/editor/ui/stage/stage.html',
+      controller: Controller,
       bindings: {
         template: '<'
       }
     });
 
+  function Controller() {
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+
+    angular.element(document.querySelector('.stage-component')).bind('wheel', function(){
+      didScroll = true;
+    });
+
+    angular.element(document.querySelector('.stage-component')).bind('touchmove', function(){
+      didScroll = true;
+    });
+
+    setInterval(function() {
+      if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+      }
+    }, 250);
+
+    function hasScrolled() {
+      var st = angular.element(document.querySelector('.stage-component')).scrollTop();
+
+      if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+      if (st > lastScrollTop){
+        $('.studio-header').removeClass('nav-down').addClass('nav-up');
+      } else {
+        $('.studio-header').removeClass('nav-up').addClass('nav-down');
+      }
+
+      lastScrollTop = st;
+    }
+  }
 }());
